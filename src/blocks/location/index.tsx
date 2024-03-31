d77c701ef2c2b35415a67135b4b3db62372d652d
Микляева Ева <miklyayeva09@mail.ru>
2024-04-01 02:38:12 +0300

    Переписывает на гугл

--- src/blocks/location/index.tsx
+++ src/blocks/location/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react';
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 import { Text } from '../../components';
 import { useDataContext } from "@/contexts";
 import styles from './index.module.css';
@@ -14,22 +13,15 @@ export const Location = () => {
   }
 
   const { map } = data;
-
   return (
     <section className={styles.location}>
       <div className={styles.text}>
            <Text className={styles.title} tag='h2'>{map.title}</Text>
            <Text className={styles.subtitle} tag='span'>{map.subtitle}</Text>
       </div>
-      <YMaps query={{ apikey: process.env.YMAP_APIKEY}}>
-          <div>
-              <Map className={styles.map} defaultState={{ center: map.center, zoom: map.zoom, behaviors: ["disable('scrollZoom')"]}}>
-                  {map.locations.map((location, index) => (
-              <Placemark key={index} geometry={location} />
-            ))}
-            </Map>
+          <div className={styles.mapContainer}>
+          <iframe className={styles.map} src="https://www.google.com/maps/d/u/1/embed?mid=1fJGAdNuCIyOo8Nfuphl3dzX4n9g4f2c&ehbc=2E312F&noprof=1" width="640" height="480"></iframe>
           </div>
-      </YMaps>
     </section>
   );
 }
