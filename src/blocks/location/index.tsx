89092cbc9919c3b61628294b927a9a4961d91452
Микляева Ева <miklyayeva09@mail.ru>
2024-04-01 20:53:35 +0300

    Ура карта

--- src/blocks/location/index.tsx
+++ src/blocks/location/index.tsx
@@ -19,9 +19,9 @@ export const Location = () => {
            <Text className={styles.title} tag='h2'>{map.title}</Text>
            <Text className={styles.subtitle} tag='span'>{map.subtitle}</Text>
       </div>
-          <div className={styles.mapContainer}>
-          <iframe className={styles.map} src="https://www.google.com/maps/d/u/1/embed?mid=1fJGAdNuCIyOo8Nfuphl3dzX4n9g4f2c&ehbc=2E312F&noprof=1" width="640" height="480"></iframe>
-          </div>
+      <div className={styles.mapContainer}>
+        <iframe className={styles.map} src="https://www.google.com/maps/embed/v1/search?key=AIzaSyCvZxzZgqnVqKMUXpcaWDLU80T42OnTDkQ&q=PFXM%2BFM%20Filyovsky%20Park%20District%2C%20Moscow%2C%20Russia%2C%20PHC6%2B92%20Khamovniki%20District%2C%20Moscow%2C%20Russia%2C%20QC3R%2BQC%20Krylatskoye%20District%2C%20Moscow%2C%20Russia%2C%20PJJ3%2BHC%20Yakimanka%20District%2C%20Moscow%2C%20RC32%2BJFW%20Strogino%20District%2C%20Moscow%2C%20Russia%2C%20QH42%2B6G8%20Presnensky%20District%2C%20Moscow" width="640" height="480"></iframe>
+      </div>
     </section>
   );
 }
