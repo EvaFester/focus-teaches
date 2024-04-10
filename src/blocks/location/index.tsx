f50098dd6bf4adb1ba1fe540dfc8bcb99e3c0fd9
Микляева Ева <miklyayeva09@mail.ru>
2024-04-10 12:48:02 +0300

    Выносит ключ в переменные

--- src/blocks/location/index.tsx
+++ src/blocks/location/index.tsx
@@ -4,23 +4,30 @@ import { Text } from '../../components';
 import { useDataContext } from "@/contexts";
 import styles from './index.module.css';
 
-export const Location = () => {
 
+const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
+export const Location = () => {
   const data = useDataContext();
 
   if (!data) {
-      return null;
+    return null;
   }
 
   const { map } = data;
+
+  const queryString = data.map.places.map(place => place.searchQuery).join(",");
+  const encodedQueryString = encodeURIComponent(queryString);
+  const embedUrl = `https://www.google.com/maps/embed/v1/search?key=${apiKey}&q=${encodedQueryString}`;
+
   return (
     <section className={styles.location}>
       <div className={styles.text}>
-           <Text className={styles.title} tag='h2'>{map.title}</Text>
-           <Text className={styles.subtitle} tag='span'>{map.subtitle}</Text>
+        <Text className={styles.title} tag='h2'>{map.title}</Text>
+        <Text className={styles.subtitle} tag='span'>{map.subtitle}</Text>
       </div>
       <div className={styles.mapContainer}>
-        <iframe className={styles.map} src="https://www.google.com/maps/embed/v1/search?key=AIzaSyCvZxzZgqnVqKMUXpcaWDLU80T42OnTDkQ&q=PFXM%2BFM%20Filyovsky%20Park%20District%2C%20Moscow%2C%20Russia%2C%20PHC6%2B92%20Khamovniki%20District%2C%20Moscow%2C%20Russia%2C%20QC3R%2BQC%20Krylatskoye%20District%2C%20Moscow%2C%20Russia%2C%20PJJ3%2BHC%20Yakimanka%20District%2C%20Moscow%2C%20RC32%2BJFW%20Strogino%20District%2C%20Moscow%2C%20Russia%2C%20QH42%2B6G8%20Presnensky%20District%2C%20Moscow" width="640" height="480"></iframe>
+        <iframe className={styles.map} src={embedUrl} width="640" height="480"></iframe>
       </div>
     </section>
   );
