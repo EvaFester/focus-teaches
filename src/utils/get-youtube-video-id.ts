056d33b3bb8abea88d0a124f924a1bf75ed9685a
Микляева Ева <miklyayeva09@mail.ru>
2024-05-14 12:47:13 +0300

    Пробный коммит

--- src/utils/get-youtube-video-id.ts
+++ src/utils/get-youtube-video-id.ts
@@ -1,6 +1,6 @@
 // Функция для извлечения идентификатора YouTube видео из URL
 export const getYoutubeVideoId = (url: string) => {
-    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&\?]*).*/;
+    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#&\?]*).*/;
     const match = url.match(regExp);
     if (match && match[2].length === 11) {
         return match[2];
